7645b765ecc235f138a41f07e2200a7b9dad532b
Nikki <thanwinnikki@gmail.com>
2022-08-30 00:46:16 +0800

    Add basic login page.

---
+++ frontend/src/components/LoginPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./LoginPage.css";
+
+const LoginPage = () => {
+  return (
+    <div className="loginBox">
+      <h1>LOGIN</h1>
+      <input type="text" placeholder="USERNAME" />
+      <input type="password" placeholder="PASSWORD" />
+
+      <div className="login-btn">Login</div>
+
+      <p className="small-text">Don't have account? Sign up here</p>
+    </div>
+  );
+};
+
+export default LoginPage;
