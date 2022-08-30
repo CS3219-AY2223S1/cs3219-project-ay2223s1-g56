505f7afea21ed280fd97c7c33b38460025ba4892
rebeccalaujx <rebeccalaujx@gmail.com>
2022-08-31 01:30:11 +0800

    minor changes

--- frontend/src/components/MatchingPage.js
+++ frontend/src/components/MatchingPage.js
@@ -7,11 +7,11 @@ import {
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import {useState} from "react";
+import './MatchingPage.css';
 
 
 function MatchingPage() {
     const [view, setView] = useState('list');
-    //const [isMatchSuccess, setIsMatchSuccess] = useState(false)
     
     const handleChange = (event, nextView) => {
         setView(nextView);
@@ -19,19 +19,12 @@ function MatchingPage() {
 
     const handleFindMatch = async () => {
         //TODO: handle find match
-        //setIsMatchSuccess(true);
     }
 
     return (
-        <div
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-            }}>
-            <Box display={"flex"} flexDirection={"column"} margin={"100"} border={1} justifyContent={"center"} width={"50%"} padding={5}>
-                <Typography align="center" fontSize={"25px"}><b>Difficulty Level:</b></Typography>
-                <br></br>
-                <br></br>
+        <div className="div">
+            <Box id="box">
+                <p id="title">Difficulty Level:</p>
                 <ToggleButtonGroup
                     orientation="vertical"
                     value={view}
@@ -39,18 +32,16 @@ function MatchingPage() {
                     onChange={handleChange}
                 >
                     <ToggleButton disableRipple value="easy">
-                        <Typography>Easy</Typography>
+                        <p>Easy</p>
                     </ToggleButton>
                     <ToggleButton disableRipple value="medium">
-                        <Typography>Medium</Typography>
+                        <p>Medium</p>
                     </ToggleButton>
                     <ToggleButton disableRipple value="hard">
-                        <Typography>Hard</Typography>
+                        <p>Hard</p>
                     </ToggleButton>
                 </ToggleButtonGroup>
-                <br></br>
-                <br></br>
-                <Box display={"flex"} flexDirection={"row"} justifyContent={"center"}>
+                <Box id="matchButton">
                     <Button variant={"outlined"} onClick={handleFindMatch}>Find a Match!</Button>
                 </Box>
                 
